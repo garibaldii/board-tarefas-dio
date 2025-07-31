9aae0806ee5449459f44dc859115ed047f7c554f
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-07-31 11:48:33 -0300

    fix:add execute func to interact with internal board's cards

--- src/main/java/br/com/dio/ui/BoardMenu.java
+++ src/main/java/br/com/dio/ui/BoardMenu.java
@@ -1,14 +1,109 @@
 package br.com.dio.ui;
 
+import br.com.dio.persistence.entity.BoardColumnEntity;
 import br.com.dio.persistence.entity.BoardEntity;
+import br.com.dio.service.BoardColumnQueryService;
+import br.com.dio.service.BoardQueryService;
 import lombok.AllArgsConstructor;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import static br.com.dio.persistence.config.ConnectionConfig.getConnection;
 
 @AllArgsConstructor
 public class BoardMenu {
 
+    private final Scanner scanner = new Scanner(System.in).useDelimiter("\n");
     private final BoardEntity entity;
 
-    public void execute(){
+    public void execute() {
+        try {
+            System.out.printf("Bem vindo ao board %s, selecione a operação desejada: ", entity.getId());
+            var option = -1;
+            while (option != 9) {
+                System.out.println("1 - Criar um card");
+                System.out.println("2 - Mover um card");
+                System.out.println("3 - Bloquear um card");
+                System.out.println("4 - Desbloquear um card");
+                System.out.println("5 - Cancelar um card");
+                System.out.println("6 - Ver Board");
+                System.out.println("7 - Visualizar Colunas com Cards");
+                System.out.println("8 - Ver Card");
+
+                System.out.println("9 - Voltar para o menu anterior");
+                System.out.println("10 - Sair");
+
+
+                option = scanner.nextInt();
+
+                switch (option) {
+                    case 1 -> createCard();
+                    case 2 -> moveCardToNextColumn();
+                    case 3 -> blockCard();
+                    case 4 -> unblockCard();
+                    case 5 -> cancelCard();
+                    case 6 -> showBoard();
+                    case 7 -> showColumn();
+                    case 8 -> showCard();
+                    case 9 -> System.out.println("Voltando para o menu anterior");
+                    case 10 -> System.exit(0);
+                    default -> System.out.println("Opção inválida, informe uma opção do menu");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    private void createCard() {
+    }
+
+    private void moveCardToNextColumn() {
+    }
+
+    private void blockCard() {
+    }
+
+    private void unblockCard() {
+    }
+
+    private void cancelCard() {
+    }
+
+    private void showBoard() throws SQLException {
+        try (var connection = getConnection()) {
+            var optional = new BoardQueryService(connection).showBoardDetails(entity.getId());
+            optional.ifPresent(b -> {
+                System.out.printf("Board [%s, %s]\n", b.id(), b.name());
+                b.columns().forEach(c -> {
+                    System.out.printf("Coluna [%s] tipo: [%s] tem %s cards\n", c.name(), c.kind(), c.cardsAmount());
+                });
+            });
+        }
+    }
+
+    private void showColumn() throws SQLException {
+        System.out.printf("Escolha uma coluna do board %s\n", entity.getName());
+        var columnsIds = entity.getBoardColumns().stream().map(BoardColumnEntity::getId).toList();
+        var selectedColumn = -1L;
+
+        while (!columnsIds.contains(selectedColumn)) {
+            entity.getBoardColumns().forEach(c -> System.out.printf("%s - %s [%s]\n", c.getId(), c.getName(), c.getKind()));
+            selectedColumn = scanner.nextLong();
+        }
+
+        try (var connection = getConnection()) {
+            var column = new BoardColumnQueryService(connection).findById(selectedColumn);
+            column.ifPresent(col -> {
+                System.out.printf("Coluna %s tipo %s\n", col.getName(), col.getKind());
+                col.getCards().forEach(card -> System.out.printf("Card %s - %s\nDescrição: %s", card.getId(), card.getTitle(), card.getDescription()));
+            });
+        }
+    }
+
+    private void showCard() {
 
     }
 }
